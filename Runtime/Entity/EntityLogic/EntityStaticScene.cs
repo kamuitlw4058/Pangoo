57539766539b5682110eba8b83a09ae2ac9cb604
kimi <199524943@qq.com>
2023-05-23 15:32:45 +0800

    * 增加实体相关代码。 * 调整默认流程

---
+++ Runtime/Entity/EntityLogic/EntityStaticScene.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pangoo{
+    public  class EntityStaticScene : EntityBase
+    {
+
+        protected override void OnInit(object userData)
+        {
+            base.OnInit(userData);
+           
+        }
+
+        protected override void OnShow(object userData)
+        {
+            base.OnShow(userData);
+
+        }
+
+        protected override void OnUpdate(float elapseSeconds, float realElapseSeconds)
+        {
+            base.OnUpdate(elapseSeconds, realElapseSeconds);
+
+        }
+
+   
+
+    }
+}
---
+++ Runtime/Entity/EnumEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace Pangoo
+{
+    public enum EnumEntity : int
+    {
+        /// <summary>
+        /// 无
+        /// </summary>
+        None = 0,
+
+
+        StaticScene = 1001,
+
+       
+    }
+}
