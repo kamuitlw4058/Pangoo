189db1775e24453c1595aad1337c0a604ab110f0
miracs <miracs@163.com>
2023-07-04 16:17:42 +0800

    解决当节点是全局节点时,Pangoo初始化还没有完成的问题。

--- Runtime/Core/EventTriggerReceiver.cs
+++ Runtime/Core/EventTriggerReceiver.cs
@@ -10,19 +10,37 @@ namespace Pangoo{
     {
         public string ConditionString;
         public UnityEvent TriggerEvent;
+        public bool Subscribed;
+        public int EventCount;
         private void OnEnable() {
-            PangooEntry.Event.Subscribe(EventTriggerEventArgs.EventId,OnEventTrigger);
+            //PangooEntry.Event.Subscribe(EventTriggerEventArgs.EventId,OnEventTrigger);
         }
+
+        private void Update()
+        {
+            if(PangooEntry.Event != null && !Subscribed)
+            {
+                PangooEntry.Event.Subscribe(EventTriggerEventArgs.EventId, OnEventTrigger);
+                Subscribed = true;
+            }
+        }
+
+
 
         void OnEventTrigger(object sender, GameEventArgs e){
             var args = e as EventTriggerEventArgs;
             if(ConditionString.Equals(args.ConditionString)){
                 TriggerEvent?.Invoke();
+                EventCount += 1;
             }
         }
 
         private void OnDisable() {
-            PangooEntry.Event.Unsubscribe(EventTriggerEventArgs.EventId,OnEventTrigger);
+            if (Subscribed)
+            {
+                PangooEntry.Event.Unsubscribe(EventTriggerEventArgs.EventId, OnEventTrigger);
+            }
+           
         }
 
 
