3a6d1557c570a980d58b3053cdfcaf57e9791d2e
纪小东 <824202750@qq.com>
2023-08-24 11:15:49 +0800

    添加CsharpCodeWriteBase基类

--- Runtime/ExcelTable/CodeWriter/CSharpCodeTableOverviewWriter.cs
+++ Runtime/ExcelTable/CodeWriter/CSharpCodeTableOverviewWriter.cs
@@ -8,15 +8,8 @@ using Newtonsoft.Json;
 
 namespace Pangoo
 {
-    public class CSharpCodeTableOverviewWriter : ICodeWriter
+    public class CSharpCodeTableOverviewWriter : CsharpCodeWriterBase
     {
-        public string FileExtension => ".cs";
-
-        private const string NoRenameAttribute = "[Obfuscation(Feature = \"renaming\", Exclude = true)]";
-        private const string NoPruneAttribute = "[Obfuscation(Feature = \"trigger\", Exclude = false)]";
-
-        List<string> m_Headers;
-
         ExcelTableData m_ExcelData;
 
         // string m_JsonDir;
@@ -31,73 +24,8 @@ namespace Pangoo
             // m_JsonDir = jsonDir;
         }
 
-        public string GetTypeName(JsonType type, IJsonClassGeneratorConfig config)
-        {
-            switch (type.Type)
-            {
-                case JsonTypeEnum.Anything: return "object";
-                case JsonTypeEnum.Array: return "List<" + GetTypeName(type.InternalType, config) + ">";
-                case JsonTypeEnum.Dictionary: return "Dictionary<string, " + GetTypeName(type.InternalType, config) + ">";
-                case JsonTypeEnum.Boolean: return "bool";
-                case JsonTypeEnum.Float: return "double";
-                case JsonTypeEnum.Integer: return "int";
-                case JsonTypeEnum.Long: return "long";
-                case JsonTypeEnum.Date: return "DateTime";
-                case JsonTypeEnum.NonConstrained: return "object";
-                case JsonTypeEnum.NullableBoolean: return "bool?";
-                case JsonTypeEnum.NullableFloat: return "double?";
-                case JsonTypeEnum.NullableInteger: return "int?";
-                case JsonTypeEnum.NullableLong: return "long?";
-                case JsonTypeEnum.NullableDate: return "DateTime?";
-                case JsonTypeEnum.NullableVector3: return "vector3?";
-                case JsonTypeEnum.NullableSomething: return "object";
-                case JsonTypeEnum.Object: return type.AssignedName;
-                case JsonTypeEnum.String: return "string";
-                case JsonTypeEnum.Vector3 : return "Vector3";
-                default: throw new System.NotSupportedException("Unsupported json type");
-            }
-        }
 
-
-        private bool ShouldApplyNoRenamingAttribute(IJsonClassGeneratorConfig config)
-        {
-            return config.ApplyObfuscationAttributes && !config.UsePascalCase;
-        }
-        private bool ShouldApplyNoPruneAttribute(IJsonClassGeneratorConfig config)
-        {
-            return config.ApplyObfuscationAttributes;
-        }
-
-        public void WriteFileStart(IJsonClassGeneratorConfig config, TextWriter sw)
-        {
-            foreach (var line in JsonClassGenerator.FileHeader)
-            {
-                sw.WriteLine("// " + line);
-            }
-            sw.WriteLine();
-            if (m_Headers != null)
-            {
-                foreach (var header in m_Headers)
-                {
-                    sw.WriteLine($"using {header};");
-                }
-            }
-            sw.WriteLine($"#if UNITY_EDITOR");
-            sw.WriteLine($"using UnityEditor;");
-            sw.WriteLine($"#endif");
-
-
-            if (ShouldApplyNoPruneAttribute(config) || ShouldApplyNoRenamingAttribute(config))
-                sw.WriteLine("using System.Reflection;");
-        }
-
-        public void WriteFileEnd(IJsonClassGeneratorConfig config, TextWriter sw)
-        {
-
-        }
-
-
-        public void WriteMainClassStart(IJsonClassGeneratorConfig config, TextWriter sw)
+        public override void WriteMainClassStart(IJsonClassGeneratorConfig config, TextWriter sw)
         {
             sw.WriteLine();
             sw.WriteLine("namespace {0}", config.Namespace);
@@ -107,7 +35,7 @@ namespace Pangoo
             sw.WriteLine("    {");
         }
 
-        public void WriteAdditionFunction(IJsonClassGeneratorConfig config, TextWriter sw)
+        public override void WriteAdditionFunction(IJsonClassGeneratorConfig config, TextWriter sw)
         {
             sw.WriteLine();
             // sw.WriteLine("         [TableList(IsReadOnly = true, AlwaysExpanded = true)]");
@@ -234,39 +162,8 @@ namespace Pangoo
 
         }
 
-        public void WriteMainClassEnd(IJsonClassGeneratorConfig config, TextWriter sw)
-        {
-            sw.WriteLine("    }");
-            sw.WriteLine("}");
-        }
 
-        public void WriteClass(IJsonClassGeneratorConfig config, TextWriter sw, JsonType type)
-        {
-            var visibility = "public";
-
-            if (!type.IsRoot)
-            {
-                if (ShouldApplyNoRenamingAttribute(config))
-                    sw.WriteLine("        " + NoRenameAttribute);
-                if (ShouldApplyNoPruneAttribute(config))
-                    sw.WriteLine("        " + NoPruneAttribute);
-                sw.WriteLine("        {0} partial class {1}", visibility, type.AssignedName);
-                sw.WriteLine("        {");
-            }
-
-            var prefix = !type.IsRoot ? "            " : "        ";
-
-            WriteClassMembers(config, sw, type, prefix);
-
-            if (!type.IsRoot)
-                sw.WriteLine("        }");
-
-            sw.WriteLine();
-        }
-
-
-
-        private void WriteClassMembers(IJsonClassGeneratorConfig config, TextWriter sw, JsonType type, string prefix)
+        public override void WriteClassMembers(IJsonClassGeneratorConfig config, TextWriter sw, JsonType type, string prefix)
         {
             foreach (var field in type.Fields)
             {
@@ -297,34 +194,6 @@ namespace Pangoo
                 else
                     sw.WriteLine(prefix + "public {0} {1} {{ get; private set; }}", field.Type.GetTypeName(), field.MemberName);
             }
-        }
-
-        private string GetTypeFromExample(string example)
-        {
-            var result = example;
-            result = result.Replace("\"", string.Empty);
-
-            var isList = result.StartsWith("list|");
-            if (isList)
-                result = result.Substring(5);
-
-            switch (result)
-            {
-                case "string":
-                case "int":
-                case "bool":
-                case "double":
-                case "DateTime":
-                case "LFloat":
-                    break;
-                default:
-                    throw new NotSupportedException("not support type from example: " + result);
-            }
-
-            if (isList)
-                result = $"List<{result}>";
-
-            return result;
         }
     }
 }
