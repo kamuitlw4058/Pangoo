204e431021f1b3acc903d756c1197068e8bd4505
王岑晗 <199524943@qq.com>
2023-12-14 22:35:45 +0800

    * 更新角色uuid 功能

--- Runtime/MetaTable/Custom/Character/CharacterDetailRowWrapper.cs
+++ Runtime/MetaTable/Custom/Character/CharacterDetailRowWrapper.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.IO;
+using System.Collections;
 using System.Collections.Generic;
 using LitJson;
 using UnityEngine;
@@ -15,169 +16,187 @@ namespace Pangoo.MetaTable
     [Serializable]
     public partial class CharacterDetailRowWrapper : MetaTableDetailRowWrapper<CharacterOverview, UnityCharacterRow>
     {
-        // [LabelText("资源ID")]
-        // [ValueDropdown("AssetPathIdValueDropdown")]
-        // [ShowInInspector]
+        [LabelText("资源id")]
+        [ValueDropdown("AssetPathIdValueDropdown")]
+        [ShowInInspector]
         // [InlineButton("ShowCreateAssetPath", SdfIconType.Plus, Label = "")]
-        // public int AssetPathId
-        // {
-        //     get
-        //     {
-        //         return UnityRow.Row?.AssetPathId ?? 0;
-        //     }
-        //     set
-        //     {
-        //         if (Row != null && Overview != null)
-        //         {
-        //             Row.AssetPathId = value;
-        //             Save();
-        //         }
-        //     }
+        public int AssetPathId
+        {
+            get
+            {
+                return UnityRow.Row?.AssetPathId ?? 0;
+            }
+            set
+            {
 
-        // }
+                UnityRow.Row.AssetPathId = value;
+                Save();
+            }
 
-        // void ShowCreateAssetPath()
-        // {
-        //     var assetOverview = GameSupportEditorUtility.GetExcelTableOverviewByConfig<AssetPathTableOverview>(Overview.Config);
-        //     var assetNewObject = AssetPathNewWrapper.Create(assetOverview, Id, ConstExcelTable.CharacterAssetTypeName, Name, afterCreateAsset: OnAfterCreateAsset);
-        //     var window = OdinEditorWindow.InspectObject(assetNewObject);
-        //     assetNewObject.Window = window;
-        // }
+        }
 
-        // public void OnAfterCreateAsset(int id)
-        // {
-        //     AssetPathId = id;
-        // }
+        [LabelText("资源Uuid")]
+        [ValueDropdown("AssetPathUuidValueDropdown")]
+        [ShowInInspector]
+        // [InlineButton("ShowCreateAssetPath", SdfIconType.Plus, Label = "")]
+        public string AssetPathUuid
+        {
+            get
+            {
+                return UnityRow.Row?.AssetPathUuid;
+            }
+            set
+            {
 
+                UnityRow.Row.AssetPathUuid = value;
+                Save();
+            }
 
-        // public IEnumerable AssetPathIdValueDropdown()
-        // {
-        //     return GameSupportEditorUtility.GetAssetPathIds(assetTypes: new List<string> { "Character" });
-        // }
+        }
 
-
-        // [ShowInInspector]
-        // public bool IsPlayer
-        // {
-        //     get
-        //     {
-        //         return Row?.IsPlayer ?? false;
-        //     }
-        //     set
-        //     {
-        //         if (Row != null && Overview != null)
-        //         {
-        //             Row.IsPlayer = value;
-        //             Save();
-        //         }
-        //     }
-        // }
+        [Button("更新AssetPathUuid通过Id")]
+        public void UpdateAssetPathUuidByAssetPathId()
+        {
+            var row = GameSupportEditorUtility.GetAssetPathById(AssetPathId);
+            if (row != null)
+            {
+                AssetPathUuid = row.Uuid;
+            }
+        }
 
 
 
-        // [ShowInInspector]
-        // [DelayedProperty]
-        // public float LinearSpeed
-        // {
-        //     get
-        //     {
-        //         return Row?.MoveSpeed ?? 1;
+        public IEnumerable AssetPathIdValueDropdown()
+        {
+            return GameSupportEditorUtility.GetAssetPathIds(assetTypes: new List<string> { "Character" });
+        }
 
-        //     }
-        //     set
-        //     {
-        //         if (Row != null && Overview != null)
-        //         {
-        //             Row.MoveSpeed = value;
-        //             Save();
-        //         }
-
-        //     }
-        // }
+        public IEnumerable AssetPathUuidValueDropdown()
+        {
+            return GameSupportEditorUtility.GetAssetPathUuids(assetTypes: new List<string> { "Character" });
+        }
 
 
-        // [ShowInInspector]
-        // [DelayedProperty]
-        // public Vector3 CameraOffset
-        // {
-        //     get
-        //     {
-        //         return Row?.CameraOffset ?? Vector3.zero;
+        [ShowInInspector]
+        public bool IsPlayer
+        {
+            get
+            {
+                return UnityRow.Row?.IsPlayer ?? false;
+            }
+            set
+            {
 
-        //     }
-        //     set
-        //     {
-        //         if (Row != null && Overview != null)
-        //         {
-        //             Row.CameraOffset = value;
-        //             Save();
-        //         }
-
-        //     }
-        // }
+                UnityRow.Row.IsPlayer = value;
+                Save();
+            }
+        }
 
 
-        // [ShowInInspector]
-        // [DelayedProperty]
-        // public float XMaxPitch
-        // {
-        //     get
-        //     {
-        //         return Row?.XMaxPitch ?? 90;
 
-        //     }
-        //     set
-        //     {
-        //         if (Row != null && Overview != null)
-        //         {
-        //             Row.XMaxPitch = value;
-        //             Save();
-        //         }
+        [ShowInInspector]
+        [DelayedProperty]
+        public float LinearSpeed
+        {
+            get
+            {
+                return UnityRow.Row?.MoveSpeed ?? 1;
 
-        //     }
-        // }
+            }
+            set
+            {
+                if (UnityRow.Row != null && Overview != null)
+                {
+                    UnityRow.Row.MoveSpeed = value;
+                    Save();
+                }
 
-        // [ShowInInspector]
-        // [DelayedProperty]
-        // public float YMaxPitch
-        // {
-        //     get
-        //     {
-        //         return Row?.YMaxPitch ?? 360;
-
-        //     }
-        //     set
-        //     {
-        //         if (Row != null && Overview != null)
-        //         {
-        //             Row.YMaxPitch = value;
-        //             Save();
-        //         }
-
-        //     }
-        // }
+            }
+        }
 
 
-        // [ShowInInspector]
-        // [DelayedProperty]
-        // [LabelText("只处理相机")]
-        // public bool CameraOnly
-        // {
-        //     get
-        //     {
-        //         return Row?.CameraOnly ?? false;
+        [ShowInInspector]
+        [DelayedProperty]
+        public Vector3 CameraOffset
+        {
+            get
+            {
+                return UnityRow.Row?.CameraOffset ?? Vector3.zero;
 
-        //     }
-        //     set
-        //     {
-        //         if (Row != null && Overview != null)
-        //         {
-        //             Row.CameraOnly = value;
-        //             Save();
-        //         }
+            }
+            set
+            {
+                if (UnityRow.Row != null && Overview != null)
+                {
+                    UnityRow.Row.CameraOffset = value;
+                    Save();
+                }
 
-        //     }
-        // }
+            }
+        }
+
+
+        [ShowInInspector]
+        [DelayedProperty]
+        public float XMaxPitch
+        {
+            get
+            {
+                return UnityRow.Row?.XMaxPitch ?? 90;
+
+            }
+            set
+            {
+                if (UnityRow.Row != null && Overview != null)
+                {
+                    UnityRow.Row.XMaxPitch = value;
+                    Save();
+                }
+
+            }
+        }
+
+        [ShowInInspector]
+        [DelayedProperty]
+        public float YMaxPitch
+        {
+            get
+            {
+                return UnityRow.Row?.YMaxPitch ?? 360;
+
+            }
+            set
+            {
+                if (UnityRow.Row != null && Overview != null)
+                {
+                    UnityRow.Row.YMaxPitch = value;
+                    Save();
+                }
+
+            }
+        }
+
+
+        [ShowInInspector]
+        [DelayedProperty]
+        [LabelText("只处理相机")]
+        public bool CameraOnly
+        {
+            get
+            {
+                return UnityRow.Row?.CameraOnly ?? false;
+
+            }
+            set
+            {
+                if (UnityRow.Row != null && Overview != null)
+                {
+                    UnityRow.Row.CameraOnly = value;
+                    Save();
+                }
+
+            }
+        }
 
     }
 }
--- Runtime/Utility/Editor/GameSupportEditorUtility.MetaTable.cs
+++ Runtime/Utility/Editor/GameSupportEditorUtility.MetaTable.cs
@@ -125,6 +125,98 @@ namespace Pangoo.MetaTable
         }
 
 
+        public static UnityAssetPathRow GetAssetPathById(int id, string packageDir = null)
+        {
+            UnityAssetPathRow ret = null;
+            var overviews = AssetDatabaseUtility.FindAsset<AssetPathOverview>(packageDir);
+            foreach (var overview in overviews)
+            {
+
+                foreach (var row in overview.Rows)
+                {
+                    if (row.Row.Id == id)
+                    {
+                        ret = row;
+                    }
+
+                }
+            }
+            return ret;
+        }
+
+        public static IEnumerable GetAssetPathIds(List<int> excludeIds = null, List<string> assetTypes = null, string packageDir = null)
+        {
+            var ret = new ValueDropdownList<int>();
+            var overviews = AssetDatabaseUtility.FindAsset<AssetPathOverview>(packageDir);
+            foreach (var overview in overviews)
+            {
+
+                foreach (var row in overview.Rows)
+                {
+                    if (assetTypes != null)
+                    {
+                        if (!assetTypes.Contains(row.Row.AssetType))
+                        {
+                            continue;
+                        }
+                    }
+
+                    if (excludeIds == null)
+                    {
+                        ret.Add($"{row.Row.Id}-{row.Name}", row.Row.Id);
+                    }
+                    else
+                    {
+                        if (!excludeIds.Contains(row.Row.Id))
+                        {
+                            ret.Add($"{row.Row.Id}-{row.Name}", row.Row.Id);
+                        }
+                    }
+                }
+            }
+            return ret;
+        }
+
+        public static IEnumerable GetAssetPathUuids(List<string> excludeUuid = null, List<string> assetTypes = null, string packageDir = null)
+        {
+            var ret = new ValueDropdownList<string>();
+            var overviews = AssetDatabaseUtility.FindAsset<AssetPathOverview>(packageDir);
+            foreach (var overview in overviews)
+            {
+
+                foreach (var row in overview.Rows)
+                {
+                    if (assetTypes != null)
+                    {
+                        if (!assetTypes.Contains(row.Row.AssetType))
+                        {
+                            continue;
+                        }
+                    }
+                    bool flag = false;
+                    if (excludeUuid == null)
+                    {
+                        flag = true;
+                    }
+                    else
+                    {
+                        if (!excludeUuid.Contains(row.UuidShort))
+                        {
+                            flag = true;
+                        }
+                    }
+
+                    if (flag)
+                    {
+                        ret.Add($"{row.UuidShort}-{row.Name}", row.Uuid);
+                    }
+
+                }
+            }
+            return ret;
+        }
+
+
 #endif
     }
 }
