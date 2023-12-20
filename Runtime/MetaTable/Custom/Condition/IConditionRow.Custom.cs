8344604eb724a89d250b223111e05b3d25aee9ca
王岑晗 <199524943@qq.com>
2023-12-20 16:00:14 +0800

    *  增加自定义行

---
+++ Runtime/MetaTable/Custom/Condition/IConditionRow.Custom.cs
@@ -0,0 +1,20 @@
+
+using System;
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using LitJson;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using System.Xml.Serialization;
+using Pangoo.Common;
+using MetaTable;
+
+namespace Pangoo.MetaTable
+{
+    public partial interface IConditionRow 
+    {
+
+    }
+}
+
