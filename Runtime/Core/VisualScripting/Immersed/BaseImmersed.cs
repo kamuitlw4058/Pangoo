fb070c32fa01a0a1583bf4a7b8955014ac901f40
纪小东 <824202750@qq.com>
2024-01-04 11:55:49 +0800

    沉浸接口

--- Runtime/Core/VisualScripting/Immersed/BaseImmersed.cs
+++ Runtime/Core/VisualScripting/Immersed/BaseImmersed.cs
@@ -1,16 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Pangoo;
 using Pangoo.Core.Services;
+using Pangoo.Core.VisualScripting;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class BaseImmersed : MonoBehaviour,IImmersed
 {
     [ReadOnly]
-    public MainService mainService = null;
+    public DynamicObject dynamicObject = null;
 
     public bool IsRunning { get; set; }
+    public bool IsInteractionEnd { get; set; }
+
+    public virtual void Start()
+    {
+        dynamicObject=GetComponent<EntityDynamicObject>().DynamicObj;
+    }
 
     public virtual void OnEnter()
     {
--- Runtime/Core/VisualScripting/Immersed/IImmersed.cs
+++ Runtime/Core/VisualScripting/Immersed/IImmersed.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public interface IImmersed
 {
     public bool IsRunning { get;}
+    public bool IsInteractionEnd { get; }
     public void OnEnter();
     public void OnExit();
 
