ead5c034f8a72a91ff2b786358e153f2acebd079
纪小东 <824202750@qq.com>
2024-01-25 13:48:11 +0800

    修改

--- Plugins/PangooCommon/Utility/MathUtility.cs
+++ Plugins/PangooCommon/Utility/MathUtility.cs
@@ -11,7 +11,15 @@ namespace Pangoo.Common
 
         public static float Remap(float input, Vector2 inputMinMax, Vector2 outputMinMax)
         {
-            return ((input - inputMinMax.x) / Mathf.Abs(inputMinMax.y - inputMinMax.x) * Mathf.Abs((outputMinMax.y - outputMinMax.x))) + outputMinMax.x;
+            if (outputMinMax.y>outputMinMax.x)
+            {
+                return ((input - inputMinMax.x) / Mathf.Abs(inputMinMax.y - inputMinMax.x) * Mathf.Abs((outputMinMax.y - outputMinMax.x))) + outputMinMax.x;
+            }
+            else
+            {
+                return Mathf.Abs((input - inputMinMax.x) / Mathf.Abs(inputMinMax.y - inputMinMax.x) * Mathf.Abs((outputMinMax.y - outputMinMax.x)) - outputMinMax.x);
+            }
+            
         }
         
         /// <summary>
@@ -30,12 +38,27 @@ namespace Pangoo.Common
                 float val = 0f;
                 if (direction==0)
                 {
-                    val=clampMinMax.y*Remap(input,inputMinMax,outputMinMax);
+                    if (outputMinMax.y>outputMinMax.x)
+                    {
+                        val=clampMinMax.y*Remap(input,inputMinMax,outputMinMax);
+                    }
+                    else
+                    {
+                        Debug.Log("当前系数:"+Remap(input,inputMinMax,outputMinMax));
+                        val=clampMinMax.y*(1-Remap(input,inputMinMax,outputMinMax));
+                    }
                 }
 
                 if (direction==1)
                 {
-                    val = clampMinMax.x*(1+Math.Abs(Remap(input,inputMinMax,outputMinMax)-1));
+                    if (outputMinMax.y > outputMinMax.x)
+                    {
+                        val = clampMinMax.x*(1+Math.Abs(Remap(input,inputMinMax,outputMinMax)-1));
+                    }
+                    else
+                    {
+                        val = clampMinMax.x*(1+Math.Abs(Remap(input,inputMinMax,outputMinMax)));
+                    }
                 }
                 
                 return Math.Clamp(val, clampMinMax.x, clampMinMax.y);
