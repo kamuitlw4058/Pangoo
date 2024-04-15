0a082265ba8b8e04bdd5fcdabdd57b7b4fdbf852
王岑晗 <199524943@qq.com>
2024-04-15 23:09:47 +0800

    * 解决部分代码依赖Hdrp的问题

---
+++ Runtime/MetaTable/Custom/Card/CardRowWrapper.cs
@@ -0,0 +1,24 @@
+#if UNITY_EDITOR
+
+using System;
+using System.Collections;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using LitJson;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using System.Xml.Serialization;
+using Pangoo.Common;
+using MetaTable;
+
+namespace Pangoo.MetaTable
+{
+    [Serializable]
+    public partial class CardRowWrapper : MetaTableRowWrapper<CardOverview,CardNewRowWrapper,UnityCardRow>
+    {
+
+    }
+}
+#endif
+
---
+++ Runtime/MetaTable/Generate/Card/ICardRow.cs
@@ -0,0 +1,24 @@
+// 本文件使用工具自动生成，请勿进行手动修改！
+
+using System;
+using System.Collections;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using LitJson;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using System.Xml.Serialization;
+using Pangoo.Common;
+using MetaTable;
+
+namespace Pangoo.MetaTable
+{
+    public partial interface ICardRow : IMetaTableRow
+    {
+
+        public string AssetPathUuid{ get; set; }
+
+    }
+}
+
